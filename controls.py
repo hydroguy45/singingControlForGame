8ab12022f5bb49f6299b538de1a92eecb0b391c4
hydroguy45 <foleychriscpfjr@gmail.com>
2017-09-13 17:38:19 -0400

    adding some requirements

---
+++ controls.py
@@ -0,0 +1,7 @@
+#This should record audio and output fft within a range with a cutoff.
+def input(milliseconds, lowFreq, highFreq, decibelCutoff):
+	print("Starting to record")
+	return None
+
+if __name__ == "__main__":
+	input(1000, 20, 800, 1)
